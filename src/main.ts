c8ecf1d6b72d7581c20ee12b74871d93e42d685c
Hamza Meknassi <elmeknassi.h@gmail.com>
2020-06-23 23:37:43 +0200

    Added uppercase characters option

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,23 @@ const app = new Vue({
         domainInput: '',
         masterPasswordInput: '',
         generatedPasswordInput: '',
-        lengthInput: 16
+        lengthInput: 16,
+        uppercaseCheckboxInput: false
     },
     methods: {
         generatePassword: function (event) {
-            const hmac = CryptoJS.HmacSHA224(this.domainInput, this.masterPasswordInput);
-            this.generatedPasswordInput = CryptoJS.SHA256(hmac.toString()).toString().substr(0, this.lengthInput);
+            if (this.domainInput === '' || this.masterPasswordInput === '')
+                return;
+
+            let pwd = CryptoJS.HmacSHA224(this.domainInput, this.masterPasswordInput).toString();
+            pwd = CryptoJS.SHA256(pwd.toString()).toString();
+            if (this.uppercaseCheckboxInput)
+                pwd = pwd.toString()
+                    .replace(new RegExp('([a-z]{1})([0-9]{2,})', 'g'),
+                        function (match: string, p1: string, p2: string) {
+                            return p1.toUpperCase() + p2;
+                        });
+            this.generatedPasswordInput = pwd.toString().substr(0, this.lengthInput);
         }
     }
 });
